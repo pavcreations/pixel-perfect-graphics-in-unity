95f2fddc2d5aa065cf762da8cc95c1b8e01048c3
Pawel Gasiorowski <pawelgasiorowskidev@gmail.com>
2021-04-14 10:40:29 +0100

    First Commit

---
+++ Assets/CanvasScaleFactorAdjuster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.U2D;
+
+public class CanvasScaleFactorAdjuster : MonoBehaviour
+{
+    public Camera MainCamera;
+
+    void Start()
+    {
+        AdjustScalingFactor();
+    }
+
+    void LateUpdate()
+    {
+        AdjustScalingFactor();
+    }
+
+    void AdjustScalingFactor()
+    {
+        GetComponent<CanvasScaler>().scaleFactor = MainCamera.GetComponent<PixelPerfectCamera>().pixelRatio;
+    }
+
+}
